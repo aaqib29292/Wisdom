7766f91a6d0be36f5a45232e0a9a39c73e6424f8
Aaqib Parvez <aaqib29292@gmail.com>
2016-10-22 11:10:51 +0530

    added bootstrap scrollspy to navigation and redesigned navbar when scrollspy activated

--- js/myscript.js
+++ js/myscript.js
@@ -1,4 +1,34 @@
 $(document).ready(function () {
+
+
+  var topoffset = 50;
+  // scrollspy
+  $('body').scrollspy({
+     target: '.navbar-fixed-top',
+     offset: topoffset
+    })
+
+
+  var hash = $(this).find("li.active a").attr("href");
+  if(hash != "#featured") {
+    $('header nav').addClass("inbody");
+  } else {
+    $('header nav').removeClass("inbody");
+  }
+
+
+
+  $('.navbar-fixed-top').on('activate.bs.scrollspy', function () {
+    var hash = $(this).find("li.active a").attr("href");
+    if(hash != "#featured") {
+      $('header nav').addClass("inbody");
+    } else {
+      $('header nav').removeClass("inbody");
+    }
+  })
+
+  // carousel
+
   $('#featured').carousel({
     interval: false
   });
