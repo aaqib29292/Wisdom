5b2c555feb92d9705d87b20b97d2f505b461aa18
Aaqib Parvez <aaqib29292@gmail.com>
2016-10-22 11:34:06 +0530

    added smooth scrolling

--- js/myscript.js
+++ js/myscript.js
@@ -26,6 +26,24 @@ $(document).ready(function () {
       $('header nav').removeClass("inbody");
     }
   })
+  console.log("loaded");
+
+  //Use smooth scrolling when clicking on navigation from CSS-Tricks
+
+  $('.navbar a[href*="#"]:not(a[href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') ===
+      this.pathname.replace(/^\//,'') &&
+      location.hostname === this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top-topoffset+2
+        }, 500);
+        return false;
+      } //target.length
+    } //click function
+  }); //smooth scrolling
 
   // carousel
 
